9f44676845e111671ea273487ff302d985f0b32c
guisiq <lopesgui01@hotmail.com>
2021-10-28 21:07:05 -0300

    desativar de periodo

--- src/main/java/br/com/itpacpalmas/api_sig_lab_itpac/controller/PeriodoController.java
+++ src/main/java/br/com/itpacpalmas/api_sig_lab_itpac/controller/PeriodoController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -51,6 +52,13 @@ public class PeriodoController {
     @DeleteMapping("/{id}")
     public void excluir(@PathVariable int id) {
         periodoRepository.deleteById(id);
+    }
+    @PatchMapping(value="/desativar/{id}")
+    public Periodo disable (@PathVariable (value = "id")Integer id){
+        Periodo periodo = periodoRepository.findById(id).get();
+        periodo.setAtivo(true);
+        periodoRepository.save(periodo);
+        return periodo;
     }
     
 }
