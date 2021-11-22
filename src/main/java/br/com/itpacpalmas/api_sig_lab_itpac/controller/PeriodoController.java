0545df3e110343edca56ebcc698f3314fc874028
guisiq <lopesgui01@hotmail.com>
2021-11-22 12:36:07 -0300

    corigindo atualizacao de data

--- src/main/java/br/com/itpacpalmas/api_sig_lab_itpac/controller/PeriodoController.java
+++ src/main/java/br/com/itpacpalmas/api_sig_lab_itpac/controller/PeriodoController.java
@@ -48,6 +48,9 @@ public class PeriodoController {
 
     @PutMapping
     public Periodo atualizaPeriodo(@RequestBody Periodo periodo) {
+        Periodo p = periodoRepository.findById(periodo.getId()).get();
+        //Mantém a data cadastro inalterada
+        periodo.setDataCadastro(p.getDataCadastro());
         return periodoRepository.save(periodo);
     }
 
