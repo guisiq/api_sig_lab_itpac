56c34807ad4ec8151117080a31a845aef55e765c
guisiq <lopesgui01@hotmail.com>
2021-11-26 08:30:02 -0300

    agendamento

--- src/main/java/br/com/itpacpalmas/api_sig_lab_itpac/entities/Agendamento.java
+++ src/main/java/br/com/itpacpalmas/api_sig_lab_itpac/entities/Agendamento.java
@@ -1,6 +1,8 @@
 package br.com.itpacpalmas.api_sig_lab_itpac.entities;
 
 import java.sql.Date;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import javax.persistence.ManyToOne;
 
@@ -38,7 +40,7 @@ public class Agendamento {
     @JoinColumn(name = "subgrupo") 
     private Subgrupo subgrupo; 
     @JsonFormat(pattern = "dd-MM-yyyy", timezone = "GMT-3")
-    private Date data;
+    private LocalDate data;
 
     private boolean ativo ;
 
@@ -84,13 +86,12 @@ public class Agendamento {
     public void setSubgrupo(Subgrupo subgrupo) {
         this.subgrupo = subgrupo;
     }
-    public Date getData() {
+    public LocalDate getData() {
         return data;
     }
-    public void setData(Date data) {
+    public void setData(LocalDate data) {
         this.data = data;
-    } 
-
+    }
     public boolean getAtivo() {
         return ativo;
     }
