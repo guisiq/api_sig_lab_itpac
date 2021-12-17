1375baa945d792bcc250f7b98eef7d5cf99bd095
guisiq <lopesgui01@hotmail.com>
2021-12-16 21:30:48 -0300

    voutando configuracao anterior

--- src/main/java/br/com/itpacpalmas/api_sig_lab_itpac/ApiSigLabItpacApplication.java
+++ src/main/java/br/com/itpacpalmas/api_sig_lab_itpac/ApiSigLabItpacApplication.java
@@ -20,13 +20,5 @@ public class ApiSigLabItpacApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiSigLabItpacApplication.class, args);
 	}
-	@Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/*").allowedOrigins("").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT").allowedHeaders("*");
-            }
-        };
-    }
+	
 }
